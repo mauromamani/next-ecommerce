54f5c8018b21676fc6be5e00c512d0ea9c3b5614
mauromamani <mauromamani.b@gmail.com>
2022-02-19 22:23:19 -0300

    feat: implement product

--- server/src/graphql/schema.ts
+++ server/src/graphql/schema.ts
@@ -2,11 +2,12 @@ import { makeSchema, fieldAuthorizePlugin } from 'nexus';
 import { join } from 'path';
 
 import * as user from './user';
+import * as product from './product';
 import * as auth from './auth';
 import * as scalars from './scalars/scalars';
 
 export const schema = makeSchema({
-  types: [scalars, user, auth],
+  types: [scalars, user, auth, product],
   outputs: {
     schema: join(__dirname, '..', '..', 'nexus', 'schema.graphql'),
     typegen: join(__dirname, '..', '..', 'nexus', 'nexus.gen.ts'),
