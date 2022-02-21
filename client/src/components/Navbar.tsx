0312f803ba1a28eefbe836b596f8c45edc6a4220
mauromamani <mauromamani.b@gmail.com>
2022-02-20 21:22:52 -0300

    chore: responsive navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -7,17 +7,42 @@ export const NavBar = () => {
     <Box
       display={'flex'}
       justifyContent={'space-between'}
-      paddingX={'20'}
+      alignItems={'center'}
+      paddingX={{ base: '4', sm: '6', md: '8' }}
       paddingTop={'2'}>
       <Box>
         <NextLink href={'/'}>
           <Link _hover={{ textDecoration: 'none' }}>
-            <Heading size={'lg'}>E-commerce</Heading>
+            <Heading size={'md'} fontWeight="light">
+              E-commerce
+            </Heading>
           </Link>
         </NextLink>
       </Box>
-      <Stack direction={['row']} spacing="4">
-        <Box>
+
+      <Stack
+        direction={['row']}
+        spacing="2"
+        textColor={'gray.600'}
+        display={{ base: 'none', md: 'inline-flex' }}>
+        <NextLink href={'/products'}>
+          <Link>
+            <Heading size={'sm'} fontWeight="semibold">
+              Hombres
+            </Heading>
+          </Link>
+        </NextLink>
+        <NextLink href={'/products'}>
+          <Link>
+            <Heading size={'sm'} fontWeight="semibold">
+              Mujeres
+            </Heading>
+          </Link>
+        </NextLink>
+      </Stack>
+
+      <Stack direction={['row']} spacing="4" alignItems="center" textColor={'gray.600'}>
+        <Box display={{ base: 'none', sm: 'block' }}>
           <MdSearch size={'30px'} />
         </Box>
         <Box>
@@ -26,6 +51,11 @@ export const NavBar = () => {
               <MdShoppingCart size={'30px'} />
             </Link>
           </NextLink>
+        </Box>
+        <Box>
+          <Box cursor={'pointer'} fontWeight="semibold">
+            Menu
+          </Box>
         </Box>
       </Stack>
     </Box>
