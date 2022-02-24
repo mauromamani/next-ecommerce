5d696962cf663e006031a4eb8960d3c826cbb594
mauromamani <mauromamani.b@gmail.com>
2022-02-24 16:42:38 -0300

    chore: building product detail page

--- client/src/pages/products/[id].tsx
+++ client/src/pages/products/[id].tsx
@@ -1,5 +1,59 @@
-const ProductItemPage = () => {
-  return <>HELLO</>;
+import { Box, Heading, Image, Text } from '@chakra-ui/react';
+import { GetServerSideProps, NextPage } from 'next';
+import { withUrqlClient } from 'next-urql';
+
+import { useProductByIdQuery } from '@/graphql/@types';
+import { PRODUCT_BY_ID } from '@/graphql/product/product.query';
+import { client, ssrCache } from '@/graphql/urql-client';
+
+interface IProps {
+  id: string;
+}
+
+const ProductItemPage: NextPage<IProps> = ({ id }) => {
+  const [{ data, error }] = useProductByIdQuery({ variables: { id } });
+
+  if (error) {
+    return <p>ERROR</p>;
+  }
+
+  console.log(data);
+
+  return (
+    <Box
+      display={'flex'}
+      justifyContent="space-between"
+      flexDirection={{ base: 'column', md: 'row' }}
+      paddingX="6"
+      paddingTop={'8'}
+      backgroundColor="gray.100">
+      <Box w={'400px'} h={'600px'}>
+        <Image src={data?.getProductById.img} />
+      </Box>
+      <Box w="40%">
+        <Heading>{data?.getProductById.title}</Heading>
+        <Text>{data?.getProductById.description}</Text>
+        <Heading>${data?.getProductById.price}</Heading>
+      </Box>
+    </Box>
+  );
 };
 
-export default ProductItemPage;
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const { id } = query;
+
+  await client?.query(PRODUCT_BY_ID, { id }).toPromise();
+  return {
+    props: {
+      id,
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
+
+export default withUrqlClient(
+  () => ({
+    url: 'http://localhost:3000',
+  }),
+  { ssr: false, neverSuspend: true },
+)(ProductItemPage);
