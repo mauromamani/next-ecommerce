b747ae39ddade76120850c9c1d0d1c194096f0ea
mauromamani <mauromamani.b@gmail.com>
2022-03-08 23:13:10 -0300

    feat: add useDisclosure hook to navbar context

--- client/src/context/navbar/NavbarCtx.tsx
+++ client/src/context/navbar/NavbarCtx.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { useDisclosure } from '@chakra-ui/react';
 import { createContext, FC, useEffect, useState } from 'react';
 
 export interface ICartProduct {
@@ -13,16 +14,23 @@ interface INavbarCtx {
   cart: ICartProduct[];
   setCartProduct: (product: ICartProduct) => void;
   removeCartProduct: (id: string) => void;
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
 }
 
 export const NavbarCtx = createContext<INavbarCtx>({
   cart: [],
   setCartProduct: () => {},
   removeCartProduct: () => {},
+  isOpen: false,
+  onOpen: () => {},
+  onClose: () => {},
 });
 
 export const NavbarProvider: FC = ({ children }) => {
   const [cart, setCart] = useState<ICartProduct[]>([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
     const cartJson = localStorage.getItem('cart');
@@ -55,7 +63,8 @@ export const NavbarProvider: FC = ({ children }) => {
   };
 
   return (
-    <NavbarCtx.Provider value={{ cart, setCartProduct, removeCartProduct }}>
+    <NavbarCtx.Provider
+      value={{ cart, setCartProduct, removeCartProduct, isOpen, onClose, onOpen }}>
       {children}
     </NavbarCtx.Provider>
   );
