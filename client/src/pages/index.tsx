4526c6e45b18489a69af1e92d52c19d06211ebf1
mauromamani <mauromamani.b@gmail.com>
2022-03-18 21:17:56 -0300

    chore: add swiper to build a slider

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,70 @@
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import { Box } from '@chakra-ui/react';
+import { GetServerSideProps } from 'next';
+import { withUrqlClient } from 'next-urql';
+import { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { ProductCard } from '@/components/products/productCard';
+import { AlertMessage } from '@/components/ui/alertMessage';
+import { useAllProductsQuery } from '@/graphql/@types';
+import { ALL_PRODUCTS } from '@/graphql/product/product.query';
+import { client, ssrCache } from '@/graphql/urql-client';
+
 function HomePage() {
-  return <></>;
+  const [{ data, error }] = useAllProductsQuery();
+
+  if (error) {
+    return (
+      <AlertMessage
+        status="error"
+        title="Ocurrió un Error!"
+        description="Contacte con el administrador del sitio"
+      />
+    );
+  }
+  return (
+    <Box padding="12" paddingTop={{ base: '0', md: '8' }} backgroundColor="gray.100">
+      <Swiper
+        spaceBetween={50}
+        // slidesPerView={2}
+        modules={[Navigation]}
+        navigation={true}
+        breakpoints={{
+          500: {
+            width: 500,
+            slidesPerView: 1,
+          },
+          800: {
+            width: 800,
+            slidesPerView: 2,
+          },
+        }}>
+        {data?.getAllProducts.map((product) => (
+          <SwiperSlide key={product.id}>
+            <ProductCard {...product} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
 }
 
-export default HomePage;
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client?.query(ALL_PRODUCTS).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
+
+export default withUrqlClient(
+  () => ({
+    url: 'http://localhost:3000',
+  }),
+  { ssr: false, neverSuspend: true },
+)(HomePage);
